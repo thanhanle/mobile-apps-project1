9192eda49dd020b7df8440889b6f8b35547f9189
tal <thanhan287@gmail.com>
2019-01-29 17:09:48 -0500

    k

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert
+} from 'react-native';
+
+// https://cryptic-shore-29745.herokuapp.com/
+
+var request = new Request('https://cryptic-shore-29745.herokuapp.com/');
+
+export default class App extends React.Component {
+
+
+  	render() {
+
+    	return (
+
+      		<View style={styles.buttonContainer}>        		
+				<Button
+					alignItems='center'
+					justifyContent='center'
+				  	outline="#841584"
+					color="#841584"
+		           	onPress={() => { 
+		           		var num = 0;
+		           		fetch(request)
+		           			.then(response => {return response.json(); })
+		           			.then(function(data) {
+		           				num = data
+		           			});
+      						// .then(data => this.setState({ data }));
+      					Alert.alert('You tapped the button: ' + num);
+		           	}}
+		           	title="Press Me">
+		        </Button>
+      		</View>
+
+    	);
+  	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
